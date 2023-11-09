b85ea9ecd292ad3fa9d73ed958ceae176467e7de
Nicklas Chiang <nicklasmc1@gmail.com>
2023-11-08 17:36:13 -0800

    Changed front page, added overview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,47 @@
-import logo from './logo.svg';
 import React from 'react';
-import './App.css';
+import './landing.css';
 
 function App() {
-  const leagues = ['League 1', 'League 2', 'League 3', 'League 4']; // Sample league names
+    const pages = ['Profile', 'Overview', 'Settings']; // Pages
 
+    return (
 
-  return (
-
-    <div className="app-container">
-    <aside className="sidebar">
-      <div className="site-icon"></div>
-      <div className="admin-username"></div>
-      <div className="statistics"></div>
-      <div className="user-reports"></div>
-    </aside>
-    <main className="main-content">
-      {leagues.map((league, index) => (
-        <div key={index} className="league-item">
-          <span className="league-name"></span>
-          <button className="edit-league"></button>
+        <div className="app-container">
+            <aside className="sidebar">
+                <div className="site-icon"></div>
+                <div className="admin-username"></div>
+                <div className="statistics"></div>
+                <div className="user-reports"></div>
+            </aside>
+            <header className="header">
+                <h1>Welcome to My App</h1>
+                <p>Discover a World of Possibilities</p>
+            </header>
+            <main className="main-content">
+                <section className="about-section">
+                    <h2>About Us</h2>
+                    <p>
+                        Welcome to our amazing application. We provide a wide range of features
+                        and services to make your life easier.
+                    </p>
+                </section>
+                <section className="features-section">
+                    <h2>Key Features</h2>
+                    <ul>
+                        <li>Profile Management</li>
+                        <li>Data Overview</li>
+                        <li>Settings Configuration</li>
+                        {/* Add more features as needed */}
+                    </ul>
+                </section>
+                <section className="get-started-section">
+                    <h2>Get Started</h2>
+                    <p>Sign in or create an account to unlock the full potential of our app.</p>
+                    <button className="get-started-button">Get Started</button>
+                </section>
+            </main>
         </div>
-      ))}
-    </main>
-  </div>
-
-  );
+    );
 }
 
 export default App;
